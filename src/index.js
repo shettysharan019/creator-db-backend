3344da587261bce4eafe17200faba4cd20c3bd41
Sharan Shetty <shettysharan019@gmail.com>
2024-12-19 13:02:43 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./config/config.env",
+});
+
+connectDB();
+
+/*
+import express from "express";
+const app = express()
+
+(async () => {
+  try {
+    const db = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("error", (err) => {
+      console.log("MongoDB connection error: ", err);
+      throw err;
+    })
+    console.log("Connected to MongoDB", db.connection.name);
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.error("ERROR: ", error)
+    throw err
+  }
+})();
+*/
